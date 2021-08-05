1cd6811628902824e2fa3633fd9f1bb6a4ce31d7
PJH <woghd8503@naver.com>
2021-08-05 12:46:52 +0900

    Python Prac

--- 다중상속01.py
+++ 다중상속01.py
@@ -2,10 +2,14 @@
 class Tiger:
     def jump(self):
         print("호랑이 점프")
+    def cry(self):
+        print("호랑이 어흥~~")
 
 class Lion:
     def bite(self):
         print("사자 물어뜯기")
+    def cry(self):
+        print("사자 으르릉~~")
 
 class Liger(Tiger, Lion):
     def play(self):
@@ -13,8 +17,5 @@ class Liger(Tiger, Lion):
 
 
 l = Liger()
-l.play()
-l.jump()
-l.bite()
-
-        
+l.cry()
+print("메서드 해석 순서:", Liger.__mro__)        
