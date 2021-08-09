bdae56e8e6c6123c899fecd13166ae0b0fff3a93
PJH <woghd8503@naver.com>
2021-08-09 11:06:16 +0900

    Final python

---
+++ 클리앙중고장터검색.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
+
+#User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
+hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
+
+for n in range(0,10):
+        #클리앙의 중고장터 주소 
+        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        print( data ) 
+        #웹브라우져 헤더 추가 
+        req = urllib.request.Request(data, \
+                                    headers = hdr)
+        data = urllib.request.urlopen(req).read()
+        #한글이 깨지는 경우 
+        page = data.decode('utf-8', 'ignore')
+        soup = BeautifulSoup(page, 'html.parser')
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+        # <span class="subject_fixed" data-role="list-title-text">
+        #   아이패드 프로 5세대 12.9(256G/WiFi/실버) 미개봉 새상품 판매해요
+        # </span>
+
+        for item in list:
+                try:
+                        title = item.text 
+                        if (re.search('맥북', title)):
+                                print(title.strip())
+                except:
+                        pass
